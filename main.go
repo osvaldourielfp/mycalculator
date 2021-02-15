0dc4f77e34128745cae8ed96273b5bcc366ea845
Osvaldo Frias <osvaldourielfp@gmail.com>
2021-02-15 01:07:42 -0600

    Operadores

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 )
 
-type calc struct{}
+type Calc struct{}
 
-// Receiver function calc.operate
-func (calc) operate(entrada string, operador string) int {
+// Receiver function Calc.Operate
+func (Calc) Operate(entrada string, operador string) int {
 	entradaLimpia := strings.Split(entrada, operador)
 	operador1 := parsear(entradaLimpia[0])
 	operador2 := parsear(entradaLimpia[1])
@@ -40,16 +40,16 @@ func parsear(entrada string) int {
 	return operador
 }
 
-func leerEntrada() string {
+func LeerEntrada() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
 }
 
 func main() {
-	entrada := leerEntrada()
-	operador := leerEntrada()
+	entrada := LeerEntrada()
+	operador := LeerEntrada()
 
-	c := calc{}
-	c.operate(entrada, operador)
+	c := Calc{}
+	c.Operate(entrada, operador)
 }
